5b15853d1f8056f5d20bc201f76db64a461a2bfb
YousofElsaka <117582037+YousofElsaka@users.noreply.github.com>
2023-04-19 23:43:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-import express from 'express'
+import express from 'express';
+const app = express();
+const students=[
 
-const app = express()
-const port = 8080;
+{
+  id: 1,
+  name: "yousof",
+  city: "Mansora"
 
-const students = ["mohmed", "ahmed", "yasser", "amr", "khaled"];
-let s = "<ui>";
-students.forEach((item) => {
-    let j = "<li>" + item + "</li>";
-    s += j;
-})
-s += "</ui>";
-const x = (req, res) => {
-    res.send(s);
+},
+{
+  id: 2,
+  name: "elsaka",
+  city: "Shebeen"
+},
+{
+id: 3,
+name: "yousri",
+city: "tanta" 
+
+},
+{
+ id: 4,
+ name: "yousof",
+ city: "cairo"
+}
+
+];
+
+
+
+ 
+
+const studentsFunction = (request , response) =>{
+let output = '<ul>'
+
+for(let i=0; i< students.length;i++){
+const student =students[i];
+output += "<li>" + student.name + "</li>";
+
+}
+
+
+output += '</ul>';
+
+response.send(output)
+
 };
 
-app.get('/students', x);
+app .get('/Students', studentsFunction)
 
+ 
+app.listen(4000, "0.0.0.0");
 
-app.listen(5000);
